37c65f45e77562e7180473d1d70ff9ffce8f3909
veganbeef <veganbeef@protonmail.com>
2024-11-13 12:35:49 -0800

    fix: update to serve on 0.0.0.0 for server deployment

--- src/main.rs
+++ src/main.rs
@@ -584,7 +584,8 @@ async fn main() -> std::io::Result<()> {
             .service(update_profile)
             .service(websocket_route) // Register the WebSocket route
     })
-    .bind(("127.0.0.1", 8080))?
+    // .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
